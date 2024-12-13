1ec789718a05945babbacdc75063726a90426151
Miguel Guevara <miguel_g1789@hotmail.com>
2024-12-12 21:06:04 -0500

    prueba

---
+++ src/data/index.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: "Devdatep Consulting EIRL (Frontend Trainee)",
+        description:
+            "I developed the company's system, together with a team of developers (Frontend) and also with the help of the backend team for the best communication regarding the data. I used new technologies such as: Zustand, React Router DOM, React Calendar, Tanstack Query, React Table, etc. I also learned how to factor and distribute the files in a correct way for the best performance in the visualization of the repository.",
+        date: "March 2023 - July 2023",
+    },
+    {
+        id: 2,
+        title: "Glinvergy SAC (Frontend Developer)",
+        description:
+            "I made the Glinseal system, a real time tracking system for RFID seals, this I did with the help of a template called Metronic. I received constant support from the backend team to understand much better the flow of the system, while I learned to use the context handler called 'Redux'. As time went by, I was given the job of creating the web pages for the clients that accessed Glinvergy's company. I also made the website of the soccer team 'Gandía CF' to which I applied NextJS and Strapi technologies, the latter as Headless CMS.",
+        date: "July 2023 - Actuality",
+    },
+    {
+        id: 3,
+        title: "Contactek Soft SAC (Frontend Developer)",
+        description:
+            "I developed the SUNASS system, with constant help from the backend team, consuming API's and interacting with the 'Scriptcase' software. I apply the Vanilla methodology for the coding of the system. Also together with other developers, we implemented ideas to improve the technologies to be used in future projects.",
+        date: "November 2023 - Actuality",
+    },
+    {
+        id: 4,
+        title: "Susana Ferrel Coaching & Consultoría (Freelance)",
+        description:
+            "I developed the website of 'Susana Ferrel Coaching & Consultancy', the page was made with the following technologies, 'React' for the Frontend and 'Strapi' which I used as CMS, constant communication with the designer, to see specific points and add good practices, for customer satisfaction.",
+        date: "September 2023 - December 2023",
+    },
+    {
+        id: 5,
+        title: "AGI Business Group (Frontend Developer)",
+        description:
+            "I developed the 'AGI Business Group' website, the page was made with the following technologies, 'React' for the Frontend and 'Strapi' which I used as CMS, constant communication with the designer, to see specific points and add good practices, for customer satisfaction.",
+        date: "September 2024 - Actuality",
+    },
+];
