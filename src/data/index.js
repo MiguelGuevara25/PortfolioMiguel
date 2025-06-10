acbf6330fa9fca34aabe43cdd1568d65969db138
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-06-10 14:57:32 -0500

    add project task-flow

--- src/data/index.js
+++ src/data/index.js
@@ -1,95 +1,105 @@
 export const data = [
-    {
-        id: 1,
-        title: "Devdatep Consulting EIRL (Frontend Trainee)",
-        description:
-            "I developed the company's system, together with a team of developers (Frontend) and also with the help of the backend team for the best communication regarding the data. I used new technologies such as: Zustand, React Router DOM, React Calendar, Tanstack Query, React Table, etc. I also learned how to factor and distribute the files in a correct way for the best performance in the visualization of the repository.",
-        date: "March 2023 - July 2023",
-    },
-    {
-        id: 2,
-        title: "Glinvergy SAC (Frontend Developer)",
-        description:
-            "I made the Glinseal system, a real time tracking system for RFID seals, this I did with the help of a template called Metronic. I received constant support from the backend team to understand much better the flow of the system, while I learned to use the context handler called 'Redux'. As time went by, I was given the job of creating the web pages for the clients that accessed Glinvergy's company. I also made the website of the soccer team 'Gandía CF' to which I applied NextJS and Strapi technologies, the latter as Headless CMS.",
-        date: "July 2023 - July 2024",
-    },
-    {
-        id: 3,
-        title: "Contactek Soft SAC (Frontend Developer)",
-        description:
-            "I developed the SUNASS system, with constant help from the backend team, consuming API's and interacting with the 'Scriptcase' software. I apply the Vanilla methodology for the coding of the system. Also together with other developers, we implemented ideas to improve the technologies to be used in future projects.",
-        date: "November 2023 - July 2024",
-    },
-    {
-        id: 4,
-        title: "Susana Ferrel Coaching & Consultoría (Freelance)",
-        description:
-            "I developed the website of 'Susana Ferrel Coaching & Consultancy', the page was made with the following technologies, 'React' for the Frontend and 'Strapi' which I used as CMS, constant communication with the designer, to see specific points and add good practices, for customer satisfaction.",
-        date: "September 2023 - December 2023",
-    },
-    {
-        id: 5,
-        title: "AGI Business Group (Frontend Developer)",
-        description:
-            "I developed the 'AGI Business Group' website, the page was made with the following technologies, 'React' for the Frontend and 'Strapi' which I used as CMS, constant communication with the designer, to see specific points and add good practices, for customer satisfaction.",
-        date: "September 2024 - Actuality",
-    },
+  {
+    id: 1,
+    title: "Devdatep Consulting EIRL (Frontend Trainee)",
+    description:
+      "I developed the company's system, together with a team of developers (Frontend) and also with the help of the backend team for the best communication regarding the data. I used new technologies such as: Zustand, React Router DOM, React Calendar, Tanstack Query, React Table, etc. I also learned how to factor and distribute the files in a correct way for the best performance in the visualization of the repository.",
+    date: "March 2023 - July 2023",
+  },
+  {
+    id: 2,
+    title: "Glinvergy SAC (Frontend Developer)",
+    description:
+      "I made the Glinseal system, a real time tracking system for RFID seals, this I did with the help of a template called Metronic. I received constant support from the backend team to understand much better the flow of the system, while I learned to use the context handler called 'Redux'. As time went by, I was given the job of creating the web pages for the clients that accessed Glinvergy's company. I also made the website of the soccer team 'Gandía CF' to which I applied NextJS and Strapi technologies, the latter as Headless CMS.",
+    date: "July 2023 - July 2024",
+  },
+  {
+    id: 3,
+    title: "Contactek Soft SAC (Frontend Developer)",
+    description:
+      "I developed the SUNASS system, with constant help from the backend team, consuming API's and interacting with the 'Scriptcase' software. I apply the Vanilla methodology for the coding of the system. Also together with other developers, we implemented ideas to improve the technologies to be used in future projects.",
+    date: "November 2023 - July 2024",
+  },
+  {
+    id: 4,
+    title: "Susana Ferrel Coaching & Consultoría (Freelance)",
+    description:
+      "I developed the website of 'Susana Ferrel Coaching & Consultancy', the page was made with the following technologies, 'React' for the Frontend and 'Strapi' which I used as CMS, constant communication with the designer, to see specific points and add good practices, for customer satisfaction.",
+    date: "September 2023 - December 2023",
+  },
+  {
+    id: 5,
+    title: "AGI Business Group (Frontend Developer)",
+    description:
+      "I developed the 'AGI Business Group' website, the page was made with the following technologies, 'React' for the Frontend and 'Strapi' which I used as CMS, constant communication with the designer, to see specific points and add good practices, for customer satisfaction.",
+    date: "September 2024 - Actuality",
+  },
 ];
 
 export const dataProjects = [
-    {
-        name: "Raíces",
-        img: "/img/projects/projectRaices.jfif",
-        url: "https://raices-p.vercel.app/",
-    },
-    {
-        name: "Pristine Pool & Spa",
-        img: "/img/projects/projectPristinePools.png",
-        url: "https://www.pristinepoolandspas.com/",
-    },
-    {
-        name: "Museos Lima",
-        img: "/img/projects/projectMuseosLima.png",
-        url: "https://museos-lima.vercel.app/",
-    },
-    {
-        name: "Cyberborders",
-        img: "/img/projects/projectCyberborder.png",
-        url: "https://www.cyberborders.com/",
-    },
-    {
-        name: "Glinseal",
-        img: "/img/projects/projectGlinseal.png",
-        url: "https://glinsealastro.vercel.app/",
-    },
-    {
-        name: "Get Energy S.L",
-        img: "/img/projects/projectGetEnergy.PNG",
-        url: "https://www.getenergy.es/",
-    },
-    {
-        name: "Grupo Comfisa SAC",
-        img: "/img/projects/projectComfisa.png",
-        url: "https://grupo-comfisa.vercel.app/",
-    },
-    {
-        name: "Susana Ferrel Coaching & Consultoría",
-        img: "/img/projects/projectSusana.png",
-        url: "https://susanaferrel.com/",
-    },
-    {
-        name: "HSE Seal",
-        img: "/img/projects/projectHSESeal.PNG",
-        url: "https://hseseal.com/",
-    },
-    {
-        name: "Festival Music",
-        img: "/img/projects/projectFestivalMusic.png",
-        url: "https://festivaldemusicaa.netlify.app/",
-    },
-    {
-        name: "Cryptocurrency Quote",
-        img: "/img/projects/projectCripto.png",
-        url: "https://coticriptos.netlify.app/",
-    },
+  {
+    name: "Raíces",
+    img: "/img/projects/projectRaices.jfif",
+    url: "https://raices-p.vercel.app/",
+  },
+
+  {
+    name: "Pristine Pool & Spa",
+    img: "/img/projects/projectPristinePools.png",
+    url: "https://www.pristinepoolandspas.com/",
+  },
+
+  {
+    name: "Task Flow",
+    img: "/img/projects/projectTaskFlow.webp",
+    url: "https://task-flow-frontend-one.vercel.app/",
+  },
+
+  {
+    name: "Museos Lima",
+    img: "/img/projects/projectMuseosLima.png",
+    url: "https://museos-lima.vercel.app/",
+  },
+
+  {
+    name: "Cyberborders",
+    img: "/img/projects/projectCyberborder.png",
+    url: "https://www.cyberborders.com/",
+  },
+
+  {
+    name: "Glinseal",
+    img: "/img/projects/projectGlinseal.png",
+    url: "https://glinsealastro.vercel.app/",
+  },
+  {
+    name: "Get Energy S.L",
+    img: "/img/projects/projectGetEnergy.PNG",
+    url: "https://www.getenergy.es/",
+  },
+  {
+    name: "Grupo Comfisa SAC",
+    img: "/img/projects/projectComfisa.png",
+    url: "https://grupo-comfisa.vercel.app/",
+  },
+  {
+    name: "Susana Ferrel Coaching & Consultoría",
+    img: "/img/projects/projectSusana.png",
+    url: "https://susanaferrel.com/",
+  },
+  {
+    name: "HSE Seal",
+    img: "/img/projects/projectHSESeal.PNG",
+    url: "https://hseseal.com/",
+  },
+  {
+    name: "Festival Music",
+    img: "/img/projects/projectFestivalMusic.png",
+    url: "https://festivaldemusicaa.netlify.app/",
+  },
+  {
+    name: "Cryptocurrency Quote",
+    img: "/img/projects/projectCripto.png",
+    url: "https://coticriptos.netlify.app/",
+  },
 ];
